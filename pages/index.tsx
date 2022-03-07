83e587d6693675622d48c744ba2c09628e12b5b5
Silvan Krähenbühl <silvan@kbuhl.ch>
2022-03-07 08:32:49 +0100

    Not working with headless ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@
 export default function Home({Component}: {Component: any}) {
   const components = require.context('../components/', true, /\.tsx$/i)
 
+  console.log(components(components.keys()[0]))
+
   return <div>
     {components(components.keys()[0]).default()}
   </div>
